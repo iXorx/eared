77fd313706e7391261581ff3f34c2d47f3c5892b
Jordi <jordi.turull@mango.com>
2025-02-03 13:48:18 +0100

    style

--- script.js
+++ script.js
@@ -8,10 +8,26 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   
   // Toggle subtitles when button is clicked
-  const toggleBtn = document.getElementById("toggleSubsBtn");
+  const toggleBtn = document.getElementById("toggleSubsBtn1");
   toggleBtn.addEventListener("click", () => {
     for (let i = 0; i < tracks.length; i++) {
       tracks[i].mode = tracks[i].mode === "showing" ? "hidden" : "showing";
+    }
+  });
+  
+  const video2 = document.getElementById("video1");
+  const tracks2 = video2.textTracks;
+  
+  // Show the first track (if available) by default
+  if (tracks2[0]) {
+    tracks2[0].mode = "showing";
+  }
+  
+  // Toggle subtitles when button is clicked
+  const toggleBtn2 = document.getElementById("toggleSubsBtn2");
+  toggleBtn2.addEventListener("click", () => {
+    for (let i = 0; i < tracks2.length; i++) {
+      tracks2[i].mode = tracks2[i].mode === "showing" ? "hidden" : "showing";
     }
   });
 });
